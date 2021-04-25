aa1162a7b563c18fc27e4956722d82b200990d9b
VladUO <81829347+VladUO@users.noreply.github.com>
2021-04-25 15:30:03 -0700

    Pushing the files to the repo before testium tests.

--- web/app.py
+++ web/app.py
@@ -1,14 +1,29 @@
 """
-John Doe's Flask API.
+Vladimir Shatalov Flask API
 """
+import config
+from flask import Flask, render_template, abort, send_from_directory  #importing stuff
 
-from flask import Flask
+app = Flask(__name__)                                   #dont know what this does
 
-app = Flask(__name__)
+forbidden = ['//', '~', '..']                           #making a list of forbidden characters
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+@app.route("/<DOCROOT:filename>")                       #rounting the request
+def request_handler(filename):                          #function for handling the request
+    if any(x in filename for x in forbidden):           #reused from P1, if any forbidden combinations in filename
+        abort(403)                                      #aborting with a 403 error
+
+    DOCROOT = config.configuration().DOCROOT            #otherwise we get the DOCROOT from the config file
+    return send_from_directory(DOCROOT, filename), 200  #and then the file from DOCROOT directory
+
+@app.errorhandler(403)                                  #error handler for a 403 error
+def forbidden_name(error):                              #function for handling the 403 error
+    return render_template('403.html'), 403             #returns 403.html file from the templates folder, and a 403 code
+
+@app.errorhandler(404)                                  #error handler for a 404 error
+def file_not_found(error):                              #function for handling the 404 error
+    return render_template('404.html'), 404             #returns 404.html file from the templates folder, and a 404 code
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
